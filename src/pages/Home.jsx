fb4756c77901df8f6f5e530fd998e096736a2e7c
Ebrima Samba <ebrima@writemedia.com>
2024-04-24 15:43:27 +0000

    added is loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,69 +3,70 @@ import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 import ClipLoader from "react-spinners/ClipLoader";
 
-
 const Home = () => {
-
   const [posts, setPosts] = useState([]);
-
+  const [isLoading, setLoading] = useState(true);
 
   const cat = useLocation().search;
 
-  useEffect(()=>{
-    const fetchData = async()=>{
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
       try {
-        setPosts([])
         const res = await axios.get(`/api/posts${cat}`);
         setPosts(res.data);
+        setLoading(false);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
+    };
     fetchData();
-  },[cat]);
+  }, [cat]);
 
-  const getText = (html) =>{
+  const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html");
     return doc.body.textContent;
+  };
+
+  const handleClick = (id) => {
+    const p = document.getElementById(`desc_${id}`);
+    p.style.height = "auto";
+  };
+
+  if (isLoading) {
+    return (
+      <ClipLoader
+        color="green"
+        loading={true}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    );
   }
 
-  const handleClick = (id)=>{
-    const p = document.getElementById(`desc_${id}`)
-    p.style.height="auto"
-  }
-
-
-  if(!posts?.length){
-    return <ClipLoader
-    color="green"
-    loading={true}
-    size={150}
-    aria-label="Loading Spinner"
-    data-testid="loader"
-  />
-  }
-  
   return (
     <div className="home">
       <div className="posts">
-        {posts.map((post)=>(
-          <div className="post" key={post?.id}>
-            <div className="img">
-              <img src={post?.img}alt=""/>
+        {!isLoading &&
+          posts.map((post) => (
+            <div className="post" key={post?.id}>
+              <div className="img">
+                <img src={post?.img} alt="" />
+              </div>
+              <div className="content">
+                <Link className="link" to={`/post/${post?.id}`}>
+                  <h1>{post?.title}</h1>
+                </Link>
+                <p id={`desc_${post?.id}`}>{getText(post?.desc)}</p>
+                <button onClick={() => handleClick(post?.id)}>
+                  Read more...
+                </button>
+              </div>
             </div>
-            <div className="content">
-              <Link className="link" to={`/post/${post?.id}`} >
-                <h1>{post?.title}</h1>
-              </Link>
-              <p id={`desc_${post?.id}`}>{getText(post?.desc)}</p>
-              <button onClick={()=>handleClick(post?.id)} >Read more...</button>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
-  )
-}
-export default Home
-
-
+  );
+};
+export default Home;
